d34c31bbed349b076c3f4a5ce42fef5c720d18e9
dallinforsyth <dallfor192@gmail.com>
2023-01-19 14:13:28 -0600

    added 3 songs to seeds db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Song.create(title: "No Role Models", album: "Forest Hills Drive", artist: "J Cole", year: "2014")
+
+Song.create(title: "1000 Blunts", album: "Sing me a lullaby, my sweet temptation", artist: "$B", year: "2022")
+
+Song.create(title: "We Own the Night", album: "Jackpot Juicer", artist: "Dance Gavin Dance", year: "2018")
